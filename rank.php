7d1918f137f5fdde11c9b4f9b17b7573bcb5f87b
長田 尚之 <n_osada@stride-web.co.jp>
2019-09-29 19:35:00 +0900

    mod

--- rank.php
+++ rank.php
@@ -1,19 +1,19 @@
 <?php
 
 function coreRate($gap, $win) {
-  $h = 0;
-  if ($win == 1) {
-    $h = 1.0;
-  } elseif ($win == -1) {
-    $h = -1.0;
-  }
-  if ($gap <= -10.0) {
-    return -1.0 + $h;
-  } elseif ($gap >= 10.0) {
-    return 1.0 + $h;
-  } else {
-    return $gap*0.1 + $h;
-  }
+    $h = 0;
+    if ($win == 1) {
+        $h = 1.0;
+    } elseif ($win == -1) {
+        $h = -1.0;
+    }
+    if ($gap <= -10.0) {
+        return -1.0 + $h;
+    } elseif ($gap >= 10.0) {
+        return 1.0 + $h;
+    } else {
+        return $gap*0.1 + $h;
+    }
 }
 
 /**
@@ -25,65 +25,55 @@ function coreRate($gap, $win) {
  * @param  int    1=world cup match
  */
 function calcRate($rA, $rB, $home=0, $win=0, $wei=0, $dbl=0) {
-  $diffA = 0;
-  $diffB = 0;
+    $diffA = 0;
+    $diffB = 0;
 
-  $rateA = $rA;
-  $rateB = $rB;
-  if ($home==1) {
-    $rateA += 3.0;
-  } elseif ($home==2) {
-    $rateB += 3.0;
-  }
-  $gap = $rateA - $rateB;
+    $rateA = $rA;
+    $rateB = $rB;
+    if ($home==1) {
+        $rateA += 3.0;
+    } elseif ($home==2) {
+        $rateB += 3.0;
+    }
+    $gap = $rateA - $rateB;
 
-  if ($win==1) {
-    $diffA += coreRate((-1)*$gap, 1);
-    $diffB += coreRate($gap, -1);
-  } elseif ($win==2) {
-    $diffA += coreRate((-1)*$gap, -1);
-    $diffB += coreRate($gap, 1);
-  } elseif ($win==0) {
-    $diffA += coreRate((-1)*$gap, 0);
-    $diffB += coreRate($gap, 0);
-  }
+    if ($win==1) {
+        $diffA += coreRate((-1)*$gap, 1);
+        $diffB += coreRate($gap, -1);
+    } elseif ($win==2) {
+        $diffA += coreRate((-1)*$gap, -1);
+        $diffB += coreRate($gap, 1);
+    } elseif ($win==0) {
+        $diffA += coreRate((-1)*$gap, 0);
+        $diffB += coreRate($gap, 0);
+    }
 
-  if ($wei) {
-    $diffA *= 1.5;
-    $diffB *= 1.5;
-  }
-  if ($dbl) {
-    $diffA *= 2;
-    $diffB *= 2;
-  }
-  $rA += $diffA;
-  $rB += $diffB;
+    if ($wei) {
+        $diffA *= 1.5;
+        $diffB *= 1.5;
+    }
+    if ($dbl) {
+        $diffA *= 2;
+        $diffB *= 2;
+    }
+    $rA += $diffA;
+    $rB += $diffB;
 
-  return [$rA, $rB];
+    return [$rA, $rB];
 }
 
-/*
-$a = 76.92;
-$b = 76.36;
-list($newA, $newB) = calcRate($a, $b, 1, 1, 0, 0);
-echo "{$newA} {$newB} \n";
-
-
-$a = 90.00;
-$b = 85.00;
-list($newA, $newB) = calcRate($a, $b, 0, 2, 0, 0);
-echo "{$a} {$b} => {$newA} {$newB} \n";
-*/
 
 
 $a = 85.07; $b = 87.32;
-list($newA, $newB) = calcRate($a, $b, 0, 1, 0, 1);
-echo "{$a} {$b} => {$newA} {$newB} \n";
-list($newA, $newB) = calcRate($a, $b, 0, 1, 1, 1);
-echo "{$a} {$b} => {$newA} {$newB} \n";
-list($newA, $newB) = calcRate($a, $b, 0, 0, 0, 1);
-echo "{$a} {$b} => {$newA} {$newB} \n";
-list($newA, $newB) = calcRate($a, $b, 0, 2, 0, 1);
-echo "{$a} {$b} => {$newA} {$newB} \n";
-list($newA, $newB) = calcRate($a, $b, 0, 2, 1, 1);
-echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 0, 1, 0, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 0, 1, 1, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 0, 0, 0, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 0, 2, 0, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 0, 2, 1, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+
+$a = 76.70; $b = 89.93;
+list($newA, $newB) = calcRate($a, $b, 1, 1, 0, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 1, 1, 1, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 1, 0, 0, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 1, 2, 0, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
+list($newA, $newB) = calcRate($a, $b, 1, 2, 1, 1); echo "{$a} {$b} => {$newA} {$newB} \n";
